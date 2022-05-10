284b2b6a30342ec026777d03ff665cc7f01f4b53
Vinicius Zanfelici <viniciuszanfelici@hotmail.com>
2022-05-10 14:46:47 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,9 @@
-function Listing () {
+import Pagination from "components/Pagination";
+
+function Listing() {
 
     return (
-        <h1>Página de Listagem</h1>
+        <Pagination />
     );
 
 }
